bc22b401f9bdadc7ae1d6d00a6679b41f9346cf9
Braian Ledantes <braianledantes@gmail.com>
2022-04-21 15:30:40 -0300

    haciento tp2ej5

---
+++ TP5_Haskell/ejercicio05.hs
@@ -0,0 +1,18 @@
+fib :: Int -> Int
+fib x
+    | x == 0 = 0
+    | x == 1 = 1
+    | otherwise = fib(x - 1) + fib(x - 2)
+
+factorial :: Int -> Int
+factorial x
+    | x == 0 = 1
+    | otherwise = x * factorial(x - 1)
+
+sumatoria :: Int -> Int -> Int
+sumatoria i n
+    | i == 1 = 1^n
+    | otherwise = (i^n) + (sumatoria (i-1) n)
+
+sucesion :: Int -> Int -> Double
+sucesion x n = sumatoria n n / factorial x
