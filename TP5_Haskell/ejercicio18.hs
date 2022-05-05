4f72ca29694d38391dd84ccaeabef0a8473892fe
Braian Ledantes <braianledantes@gmail.com>
2022-05-05 15:36:30 -0300

    eliminado ejercicios no hechos

--- TP5_Haskell/ejercicio18.hs
+++
@@ -1,6 +0,0 @@
--- definir una funcion
--- justificarCentro :: Int -> String -> String
--- de forma que justificarCentro n st son devuelva un String de longitud n en el
--- cual se han agregado espacios en ambos extremos de st de modo que quede centrado
--- Validar todos los casos
--- dar una solucioin utilizando la clausula where y otra sin ella.
