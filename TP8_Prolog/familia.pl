e286c32e64aa5293cce615f559ac88340116c9b4
Braian Ledantes <braianledantes@gmail.com>
2022-06-01 21:00:05 -0300

    add familia.pl original

---
+++ TP8_Prolog/familia.pl
@@ -0,0 +1,72 @@
+%Progenitor
+progenitor(juan, ana).
+progenitor(maria, ana).
+progenitor(pedro,pablo).
+progenitor(maria,pablo).
+progenitor(juan,luis).
+progenitor(maria,luis).
+progenitor(pedro,luciano).
+progenitor(laura,luciano).
+progenitor(laura,ariel).
+progenitor(pedro,ariel).
+progenitor(tomas,pedro).
+progenitor(jose,juan).
+progenitor(daniel,laura).
+progenitor(andrea,laura).
+progenitor(eugenia,maria).
+progenitor(martin,maria).
+progenitor(eugenia,rodolfo).
+progenitor(martin,rodolfo).
+progenitor(eugenia,sandra).
+progenitor(martin,sandra).
+progenitor(rodolfo,damian).
+progenitor(claudia,damian).
+progenitor(norma,andrea).
+progenitor(carlos,andrea).
+progenitor(norma,cecilia).
+progenitor(carlos,cecilia).
+progenitor(cecilia,gabriel).
+progenitor(javier,gabriel).
+progenitor(jorge,carlos).
+progenitor(silvia,carlos).
+
+
+% Femenino o Masculino
+
+femenino(ana).
+femenino(maria).
+femenino(laura).
+femenino(eugenia).
+femenino(andrea).
+femenino(sandra).
+femenino(claudia).
+femenino(norma).
+femenino(cecilia).
+femenino(silvia).
+
+
+masculino(juan).
+masculino(pedro).
+masculino(pablo).
+masculino(luis).
+masculino(luciano).
+masculino(ariel).
+masculino(tomas).
+masculino(jose).
+masculino(daniel).
+masculino(martin).
+masculino(rodolfo).
+masculino(damian).
+masculino(carlos).
+masculino(gabriel).
+masculino(javier).
+masculino(jorge).
+
+%Casados
+casado(juan, maria).
+casado(pedro, laura).
+casado(eugenia, martin).
+casado(rodolfo, claudia).
+casado(daniel, andrea).
+casado(norma,carlos).
+casado(cecilia,javier).
